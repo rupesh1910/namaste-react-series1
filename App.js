61840b66cd7ba1fff16025741e36631a47eded46
rupesh <rupesh19101989@gmail.com>
2023-07-11 22:56:55 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React  from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div",{id:"parent"},React.createElement("div",
 {id:"child"}
-,[React.createElement("h1",{id:"hed"},"i'm nested html"),React.createElement("h2",{id:"hed"},"i'm nested html")]))
+,[React.createElement("h1",{id:"hed"},"i'm nested html"),React.createElement("h2",{id:"hed"},"i'm neasted html")]))
 
 console.log(parent)
 
